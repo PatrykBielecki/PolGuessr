712032281bea88275f6041401b8b7d05c33f82ec
Patryk Bielecki <patryk.bielecki@nexpertis.pl>
2022-12-07 21:41:08 +0100

    Game without instruction screen

---
+++ src/UserPin.java
@@ -0,0 +1,20 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+public class UserPin extends JLabel {
+
+    public int xPos=0, yPos=0;
+    boolean moveable = true, placed = false;
+    UserPin() {
+        try {
+            setIcon(new ImageIcon(ImageIO.read(new File("resources/userPin.png"))));
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        setSize(25,40);
+    }
+
+}
